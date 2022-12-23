01616b4e480664d27463f510575c6e7d33c1d8c1
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-23 21:03:06 +0300

    navbar css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
-import './App.css';
-import { Home } from './component/Home';
+import React, { component } from "react";
+import { BrowserRouter, Routes, Route } from "react-route-dom";
+import "./App.css";
+import { Home } from "./component/Home";
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" component={Home} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import { Navbar } from './Navbar'
-import { Product } from './Product'
+import React from "react";
+import "../css/Home.css";
+import { Navbar } from "./Navbar";
+import { Product } from "./Product";
 export const Home = () => {
   return (
     <div>
-        <Navbar/>
-        <Product/>
+      <Navbar />
+      <Product />
     </div>
-  )
-}
+  );
+};
